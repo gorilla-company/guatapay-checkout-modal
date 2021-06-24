920f1486e385b54825a23933957e3cc882f6ed68
Pablo Marchesini <pablo.marchesini@emerios.com>
2021-06-24 11:59:17 -0300

    status handling

--- ModoModal.js
+++ ModoModal.js
@@ -1,17 +1,18 @@
 
 let initialized = false;
-let currentStatus = 'IDLE'
+let mockStatus = 'STARTED';
+let currentStatus = 'STARTED';
 
 function buildHtml(qrCode) {
   let link = document.createElement('link');
-  link.rel = 'stylesheet'; 
+  link.rel = 'stylesheet';
   link.type = 'text/css';
   link.href = './styles.css';
   document.head.appendChild(link);
 
   // Section
   let section = createElementWithClass("section", "modal-wrapper");
-  section.id="main_modal";
+  section.id = "main_modal";
   let header = document.createElement("header");
   let divWrapper = createElementWithClass("div", "modal-header-wrapper");
   divWrapper.innerHTML = '<img src="./img/logo.jpg" alt="logo">';
@@ -36,6 +37,9 @@ function buildHtml(qrCode) {
   navBar.appendChild(step5Ball);
 
   // create steps containers
+  
+  // STEP 1
+
   let step1Div = createElementWithClass("div", "modal-body-wrapper");
   step1Div.id = "step-1";
   step1Title = document.createElement("p");
@@ -49,20 +53,61 @@ function buildHtml(qrCode) {
   step1Div.appendChild(qrContainer);
   step1Div.appendChild(disclaimerQuestion);
 
+  // STEP 2
 
   let step2Div = createElementWithClass("div", "modal-body-wrapper");
   step2Div.classList.add("hide");
   step2Div.id = "step-2";
 
+  step2Title = document.createElement("p");
+  step2Title.innerHTML = "Estamos esperando confirmaci\u00F3n";
+
+  let divLoading = document.createElement("div");
+  divLoading.innerHTML = '<img src="./img/loading.gif" alt="loading">';
+
+  let step2Text = document.createElement("p");
+  step2Text.innerHTML = "Eleg\u00ED el medio de pago y confirm\u00E1 la transacci\u00F3n"
+
+  step2Div.appendChild(step2Title);
+  step2Div.appendChild(divLoading);
+  step2Div.appendChild(step2Text);
+
+  // STEP 3
+
   let step3Div = createElementWithClass("div", "modal-body-wrapper");
   step3Div.classList.add("hide");
   step3Div.id = "step-3";
 
+  let step3Text = document.createElement("p");
+  step3Text.innerHTML = "Pago en proceso"
+
+  let divLoadingStep3 = document.createElement("div");
+  divLoadingStep3.innerHTML = '<img src="./img/loading.gif" alt="loading">';
+
+  let step3TextUpper = document.createElement("p");
+  step3TextUpper.innerHTML = "Estamos procesando tu pago,"
+
+  let step3TextLower = document.createElement("p");
+  step3TextLower.innerHTML = "te pedimos aguardar, puede demorar unos segundos."
+
+  step3Div.appendChild(step3Text);
+  step3Div.appendChild(divLoadingStep3);
+  step3Div.appendChild(step3TextUpper);
+  step3Div.appendChild(step3TextLower);
+
+  // STEP 4
 
   let step4Div = createElementWithClass("div", "modal-body-wrapper");
   step4Div.classList.add("hide");
   step4Div.id = "step-4";
 
+  let step4Title = document.createElement("p");
+  step4Title.innerHTML = "\u00A1Listo!"
+  let divImgStep4 = document.createElement("div");
+  divImgStep4.innerHTML = '<img class="modal-ok-img" src="./img/ok.png" alt="ok">';
+
+  step4Div.appendChild(step4Title);
+  step4Div.appendChild(divImgStep4);
 
   // append items to hierarchy
   header.appendChild(divWrapper);
@@ -106,7 +151,7 @@ function closeModal() {
   clearAsyncInterval();
   let modal = document.getElementById('main_modal');
   document.body.removeChild(modal);
-  initialized = false; 
+  initialized = false;
 }
 
 
@@ -164,7 +209,7 @@ const setAsyncInterval = (cb, interval) => {
 };
 
 const clearAsyncInterval = () => {
-  if(asyncIntervals.length > 0) {
+  if (asyncIntervals.length > 0) {
     asyncIntervals.forEach((item, index) => {
       if (asyncIntervals[index]) {
         asyncIntervals[index] = false;
@@ -175,6 +220,38 @@ const clearAsyncInterval = () => {
 
 const getStatus = async () => {
   let response = await getData('https://api.develop.playdigital.com.ar/ecommerce/payment-intention/c529ce02-3b55-4e1b-b63b-55b5f94169a6?mocked_status={status}'
-  .replace('{status}', currentStatus));
-  console.log(response);
+    .replace('{status}', mockStatus));
+    setModalStatus(response.status);
+}
+
+const setModalStatus = (status) => {
+  if(status == currentStatus) {
+    return;
+  }
+  currentStatus = status;
+  switch (status) {
+    case 'STARTED':
+      handleClick(1);
+      break;
+    case 'PROCESSING':
+      handleClick(2);
+      break;
+    case 'PAYING':
+      handleClick(3);
+      break;
+    case 'PAYMENT_READY':
+      handleClick(4);
+      break;
+    case 'PAYMENT_DENIED':
+
+      break;
+    case 'ERROR':
+
+      break;
+    case 'EXPIRED':
+
+      break;
+    default:
+      break;
+  }
 }

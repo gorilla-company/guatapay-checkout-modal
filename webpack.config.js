4fcc3168efcd6fcc79f200ab869be643b5f91c53
Pablo Marchesini <pablo.marchesini@emerios.com>
2021-08-18 11:01:51 -0300

    webpack config for env and prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const Dotenv = require('dotenv-webpack');
 
-module.exports = {
+module.exports = env => ({
 
   entry: './src/ModoModal.js',
 
@@ -37,13 +37,15 @@ module.exports = {
     ],
   },
   plugins: [
-    new Dotenv(),
+    new Dotenv({
+			path: `./.env.${env.environment}`
+		}),
   ],
   
- mode: 'development'
+ mode: env.mode
   // mode: '',
   //  The 'mode' option has not been set, webpack will fallback to 'production' for this value.
   //  Set 'mode' option to 'development' or 'production' to enable defaults for each environment.
   //  You can also set it to 'none' to disable any default behavior. Learn more: https://webpack.js.org/configuration/mode/
   
-};
+});

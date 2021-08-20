02c63420ae1e0e6bf0334a4f66c835b2f8af759a
Pablo Marchesini <pablo.marchesini@emerios.com>
2021-08-20 12:54:27 -0300

    fix deeplink url

--- src/services/deeplink.service.js
+++ src/services/deeplink.service.js
@@ -5,7 +5,7 @@ function redirectToDeeplink(modalObject) {
 
 function buildDeepLink(modalObject) {
   let deeplinkObject = modalObject.deeplink;
-  let url = `${deeplinkObject.url}qr={qr}&callback={callbackURL}&callbackSuccess={callbackURLSuccess}`;
+  let url = `${deeplinkObject.url}?qr={qr}&callback={callbackURL}&callbackSuccess={callbackURLSuccess}`;
   url = url
     .replace("{qr}", modalObject.qrString)
     .replace("{callbackURL}", deeplinkObject.callbackURL)

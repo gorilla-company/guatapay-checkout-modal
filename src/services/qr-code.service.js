06f80783756784fa7fb9b9a310ede33e3ac80c66
Pablo Marchesini <pablo.marchesini@emerios.com>
2021-11-08 11:03:17 -0300

    qr fix

--- src/services/qr-code.service.js
+++ src/services/qr-code.service.js
@@ -11,8 +11,8 @@ function loadQr(qrCode) {
 
 function generateQr(qrString) {
   const qrCode = new QRCodeStyling({
-    width: 200,
-    height: 200,
+    width: 220,
+    height: 220,
     data: qrString,
     margin: 0,
     imageOptions: {
